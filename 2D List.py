8a32cabb5460a180eee1cf39542c2694e6219111
44240 <44240@PC004145.coll.lr.local>
2015-01-13 12:29:01 +0000

    Prints a 2D list in the form of a table.

---
+++ 2D List.py
@@ -0,0 +1,14 @@
+players = [
+    ["k1llm4chine", 49, 51],
+    ["bob2247", 5, 99],
+    ["hAxOr12", 72, 30]
+]
+
+print("-" * 22)
+for player in players:
+    print("|{0:<12}|{1:<3}|{2:<3}|".format(player[0], player[1], player[2]))
+    print("-" * 22)
+
+
+
+
